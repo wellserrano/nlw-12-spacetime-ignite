656578c3a70b63e3cb66d6fbfb53b467946a67eb
wellserrano <66437201+wellserrano@users.noreply.github.com>
2023-05-18 14:29:36 -0300

    fix: packages restored from commit 5537a17

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -2,10 +2,6 @@ module.exports = function (api) {
   api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-<<<<<<< HEAD
     plugins: ['nativewind/babel', require.resolve('expo-router/babel')],
-=======
-    plugins: ['nativewind/babel'],
->>>>>>> 7a56d27 (Aula 01)
   }
 }
