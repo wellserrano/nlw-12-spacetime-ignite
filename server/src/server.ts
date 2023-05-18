302fcc0bd8fdbbcf34689c35ae82a4da060c2d1b
wellserrano <66437201+wellserrano@users.noreply.github.com>
2023-05-18 15:08:41 -0300

    server recover attempt1

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-import 'dotenv/config'
-
-import fastify from 'fastify'
-import cors from '@fastify/cors'
-import jwt from '@fastify/jwt'
-import { memoriesRoutes } from './routes/memories'
-import { authRoutes } from './routes/auth'
-
-const app = fastify()
-
-app.register(cors, {
-  origin: true,
-})
-
-app.register(jwt, {
-  secret: 'spacetime',
-})
-
-app.register(authRoutes)
-app.register(memoriesRoutes)
-
-app
-  .listen({
-    port: 3333,
-    host: '0.0.0.0',
-=======
 import fastify from 'fastify'
 import { PrismaClient } from '@prisma/client'
 
@@ -40,7 +13,6 @@ app.get('/users', async () => {
 app
   .listen({
     port: 3333,
->>>>>>> 7a56d27 (Aula 01)
   })
   .then(() => {
     console.log('🚀 HTTP server running on port http://localhost:3333')
