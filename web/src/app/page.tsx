c1b5c271da69161dfa74b2174715c99b106133ce
wellserrano <66437201+wellserrano@users.noreply.github.com>
2023-05-18 13:24:45 -0300

    feat: main page structure

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,7 @@
+import { User } from 'lucide-react'
+import Image from 'next/image'
+import nlwLogo from '../assets/nlw-spacetime-logo.svg'
+
 export default function Home() {
   return (
     <div className="grid min-h-screen grid-cols-2">
@@ -6,6 +10,50 @@ export default function Home() {
         <div className="absolute right-0 top-1/2 h-[18rem] w-[32.875rem] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
         {/* Stripes divisor */}
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+
+        {/* Login */}
+        <a href="" className="group flex items-center gap-3 text-left">
+          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400 transition-transform group-hover:scale-105">
+            <User className="h-5 w-5 text-gray-500" />
+          </div>
+          <p className="max-w-[9rem] text-sm leading-snug transition-colors group-hover:text-gray-50">
+            <span>Crie sua conta</span> e salve suas memórias!
+          </p>
+        </a>
+
+        {/* Hero */}
+        <div className="space-y-5">
+          <Image src={nlwLogo} alt="" />
+
+          <div className="max-w-[26.25rem] space-y-1">
+            <h1 className="text-4xl font-bold leading-tight text-gray-50">
+              Sua cápsula do tempo
+            </h1>
+            <p className="text-lg leading-relaxed">
+              Colecione momentos marcantes da sua jornada e compartilhe (se
+              quiser) com o mundo!
+            </p>
+          </div>
+          <a
+            href=""
+            className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm font-bold uppercase leading-none text-black hover:bg-green-600"
+          >
+            CADASTRAR LEMBRANÇA
+          </a>
+        </div>
+
+        {/* Footer */}
+        <div className="text-sm leading-relaxed text-gray-200">
+          Feito com 💜 no NLW da{' '}
+          <a
+            href="https://rocketseat.com.br"
+            target="_blank"
+            rel="noreferrer"
+            className="underline hover:text-gray-100"
+          >
+            Rocketseat
+          </a>
+        </div>
       </div>
 
       <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
