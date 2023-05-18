8acf51e042e318fc446bf47f88b3cc20917036c9
wellserrano <66437201+wellserrano@users.noreply.github.com>
2023-05-18 11:27:32 -0300

    fix: packages recall

--- web/next.config.js
+++ web/next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-<<<<<<< HEAD
-const nextConfig = {
-  images: {
-    domains: ['avatars.githubusercontent.com'],
-  },
-}
-=======
 const nextConfig = {}
->>>>>>> 7a56d27 (Aula 01)
 
 module.exports = nextConfig
