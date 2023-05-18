2dc0c373854ff4d630125fa11eeb1bcc34acdb1f
wellserrano <66437201+wellserrano@users.noreply.github.com>
2023-05-18 19:05:07 -0300

    feat: auth routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,24 @@
-import fastify from 'fastify'
-import { memoriesRoutes } from './routes/memories'
+import 'dotenv/config'
 
+import fastify from 'fastify'
+import jwt from '@fastify/jwt'
 import cors from '@fastify/cors'
+
+import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 
 app.register(cors, {
   origin: true,
 })
+
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
 app.register(memoriesRoutes)
+app.register(authRoutes)
 
 app
   .listen({
