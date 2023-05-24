6b094f909f983018509e425d652f82f3f136028d
wellserrano <66437201+wellserrano@users.noreply.github.com>
2023-05-24 19:12:43 -0300

    feat: image and video upload

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,11 +3,21 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import jwt from '@fastify/jwt'
 import cors from '@fastify/cors'
+import multipart from '@fastify/multipart'
+import fStatic from '@fastify/static'
 
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
+
+app.register(multipart)
+app.register(fStatic, {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true,
@@ -18,6 +28,7 @@ app.register(jwt, {
 })
 
 app.register(authRoutes)
+app.register(uploadRoutes)
 app.register(memoriesRoutes)
 
 app
